93d8fd4738b863eabbb9a8b6898ef3ab4c87e0a6
lizconlan <lizconlan@gmail.com>
2011-11-08 12:03:35 +0000

    added contributions_by_member to Section

--- models/section.rb
+++ models/section.rb
@@ -25,4 +25,23 @@ class Section
   key :fragment_ids, Array
   key :name, String
   key :sequence, Integer
+  
+  def contributions_by_member(member_name)
+    contribs = []
+    contrib = []
+    last_id = ""
+    paras = Paragraph.by_member_and_fragment_id_start(member_name, id).all
+    paras.each do |para|
+      unless para.contribution_id == last_id
+        unless contribs.empty? and contrib.empty?
+          contribs << contrib
+          contrib = []
+        end
+      end
+      contrib << para
+      last_id = para.contribution_id
+    end
+    contribs << contrib unless contrib.empty?
+    contribs
+  end
 end
